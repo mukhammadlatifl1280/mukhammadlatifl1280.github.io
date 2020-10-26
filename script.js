eea18aa6548b93c6482bcb19003ae3280d048c4f
mukhammadlatifl1280 <73484322+mukhammadlatifl1280@users.noreply.github.com>
2020-10-26 10:58:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Mukhammadlatif Latipov"
+let userName = "mukhammadlatifl1280";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
